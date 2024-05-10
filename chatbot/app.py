5f54a05f9ca14f0076df95bc4c45b21c0dc658df
karthikyeluripati <sy493@njit.edu>
2024-05-10 17:29:57 -0400

    updated with openai

--- chatbot/app.py
+++ chatbot/app.py
@@ -6,6 +6,8 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 #Langsmith tracking
 os.environ["LANGCHAIN_TRACKING_V2"]="true"
